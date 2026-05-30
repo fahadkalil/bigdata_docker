a3debf2bd336b6f274b45b436d8874a437febc1e
Fahad Kalil <4945023+fahadkalil@users.noreply.github.com>
2026-05-30 18:29:24 -0300

    Adiciona implementação de pipeline Kafka para MongoDB no PyFlink com serialização e deserialização de json no Flink

---
+++ examples/04_apiclima_kafka_mongodb/clima_jobpipeline_v2.py
@@ -0,0 +1,50 @@
+## REFERENCIA
+## https://raw.githubusercontent.com/decodableco/examples/refs/heads/main/pyflink/pyflink_hello_world.py
+
+from pyflink.datastream import StreamExecutionEnvironment
+from pyflink.table import StreamTableEnvironment
+
+def kafka_mongodb_pipeline():
+    env = StreamExecutionEnvironment.get_execution_environment()
+    env.set_parallelism(1)
+
+    t_env = StreamTableEnvironment.create(stream_execution_environment=env)    	
+
+    t_env.execute_sql("""
+    CREATE TABLE kafka_source (
+     `latitude` STRING,
+     `longitude` STRING,
+     `timezone` STRING,
+     `hourly_time` STRING,
+     `hourly_temperature` STRING
+    ) WITH (
+    'connector' = 'kafka',
+    'topic' = 'apiclima',
+    'properties.bootstrap.servers' = 'kafka:9092',
+    'properties.group.id' = 'apiclimaGroup',
+    'scan.startup.mode' = 'earliest-offset',
+    'format' = 'json'
+    )""")
+   
+    t_env.execute_sql("""
+    CREATE TABLE mongo_sink (    
+     `latitude` STRING,
+     `longitude` STRING,
+     `timezone` STRING,
+     `hourly_time` STRING,
+     `hourly_temperature` STRING
+    ) WITH (
+        'connector' = 'mongodb',
+        'uri' = 'mongodb://mongo:27017',
+        'database' = 'clima',
+        'collection' = 'apiclima'
+    )""")
+
+    ## gerar o insert + select para ativar o pipeline
+    t_env.execute_sql("""
+    INSERT INTO mongo_sink
+     SELECT * FROM kafka_source
+    """)
+
+if __name__ == '__main__':    
+    kafka_mongodb_pipeline()
